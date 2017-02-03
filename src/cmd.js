00edcf865f5f6b203789781c55b6423ee3a61bc5
zoltan.fodor <zoltan.fodor@pitechnologies.ro>
2017-02-03 16:04:31 +0200

    Error display fixes.

--- src/cmd.js
+++ src/cmd.js
@@ -9,18 +9,23 @@ function padd(str, length = process.stdout.columns) {
 module.exports = {
 
   error(error) {
+
     let length = process.stdout.columns;
 
-    let firstBreak = error.message.indexOf("\n") || error.message.length;
+    let firstBreak = error.message.indexOf("\n");
+    if (firstBreak === -1) {
+      firstBreak = error.message.length;
+    }
+
     let errorHead = (" " + error.message.substr(0, firstBreak)).match(new RegExp(`.{1,${length - 1}}`, "g"));
     let errorBody = error.message.substr(firstBreak).replace(/\n(.)/g, "\n  • ".red + "$1");
 
-    console.error("\n !ERROR ".bgRed + "▂".repeat(length - 8).red);
+    console.error("\n !ERROR ".bgRed + "•".repeat(length - 8).red);
     console.error(padd(errorHead.join("\n")).bgRed.white);
     console.error(errorBody);
 
-    console.error("\n !STACK TRACE ".bgYellow.black + "▂".repeat(length - 14).yellow);
-    console.error(error.stack.substr(error.message.length + 7).replace(/    /g, "  • ".yellow));
+    console.error("\n !STACK TRACE ".bgYellow.black + "•".repeat(length - 14).yellow);
+    console.error(error.stack.substr(error.stack.indexOf("    at ")).replace(/    /g, "  • ".yellow));
   },
 
   warning(msg) {
