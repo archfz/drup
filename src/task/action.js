8658769a708d5c61f6524815fcb2f0a65572556b
zoltan.fodor <zoltan.fodor@pitechnologies.ro>
2017-02-07 20:33:56 +0200

    Refactors action base class.

--- src/task/action.js
+++ src/task/action.js
@@ -1,62 +1,85 @@
 "use strict";
 
+const utils = require("../utils");
+
 module.exports = class Action {
 
   constructor() {
-    this._completed = false;
-  }
+    this.params = {};
+    this.parentActions = [];
+    this.completedActions = [];
 
-  complete(callback) {
-    if (this._completed) {
-      return false;
-    }
-
-    this._fire('_doWork', (succeeded) => {
-      if (succeeded && typeof this._onCompleted === "function") {
-        this._onCompleted();
-      } else if (typeof this._onFailed === "function") {
-        this._onFailed();
-      }
-
-      callback(succeeded);
+    this.promise = new Promise((resolve) => {
+      this._resolve = resolve;
+    }).catch((err) => {
+      this._chainFailed(err);
     });
-    return true;
   }
 
-  revert(callback) {
-    if (this._completed) {
-      return false;
-    }
-
-    this._fire('_doRevert', callback, false);
-    return true;
+  get resultKey() {
+    return null;
   }
 
-  _fire(method, callback, completedModifier = true) {
-    if (typeof this[method] !== "function") {
-      throw new Error(`Action: an action must implement the ${method} method.`);
+  start(params) {
+    this.params = params;
+
+    Promise((resolve) => {
+      this.complete();
+      resolve();
+    });
+
+    return this.promise;
+  }
+
+  resolve(value) {
+    if (this.resultKey) {
+      this.params[this.resultKey] = value;
     }
 
-    let completion = this[method]();
+    this._resolve(this.params);
+  }
 
-    if (typeof completion === "boolean") {
-      this._completed = (completion === completedModifier);
-      callback(completion);
-    } else if (completion instanceof Promise) {
-      completion.then(
-        () => {
-          this._completed = completedModifier;
-          callback(true);
-        },
-        () => {
-          this._completed = !completedModifier;
-          callback(false);
-        }
-      ).catch(error => {
-        console.error(error);
-      });
-    } else {
-      throw new Error(`Action: ${method} method must return boolean or a Promise.`);
+  complete(params) {
+    utils.mustImplement(this, "start");
+  }
+
+  revert() {
+
+  }
+
+  after(action, condition = () => true) {
+    this.parentActions.push(action);
+
+    action.promise.then((params) => {
+      if (condition(params[action.resultKey])) {
+        this._previousCompleted(action, params);
+      }
+    });
+
+    return this;
+  }
+
+  static after() {
+    return new (this.constructor)().after.call(arguments);
+  }
+
+  _chainFailed(err) {
+    this.revert(err);
+
+    this.parentActions.forEach((action) => {
+      action._chainFailed(err);
+    });
+  }
+
+  _previousCompleted(action, params) {
+    if (action.resultKey) {
+      this.params[action.resultKey] = params[action.resultKey];
+    }
+
+    this.completedActions.push(action);
+
+    if (this.parentActions.length === this.completedActions.length) {
+      this.complete(this.params);
     }
   }
 
