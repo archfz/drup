2828a176f52ac4155bbb133b454e62f725f71331
FodorZoltan <fodor18zoltan@gmail.com>
2017-02-24 18:53:28 +0200

    Implements drush operation for drush service.

--- src/projects/types/drupal/services/drush/index.js
+++ src/projects/types/drupal/services/drush/index.js
@@ -1,21 +1,53 @@
 "use strict";
 
 const Service = require("../../../../../environment/service_base");
+const Command = require("../../../../../system/system_command");
 
 /**
  * @id drush
  * @group misc
  * @label Drush
  */
-module.exports = class DrushService extends Service {
+class DrushService extends Service {
 
   _composeDocker() {
     return {
       image: "drush/drush",
-      volumes: [
-        `/${this._dir("PROJECT")}:/var/www/html`
+      volumes_from: [
+        this._getWeb().ann("id"),
       ]
     };
   }
 
-};
+  getOperations() {
+    return [
+      {
+        baseName: "drush",
+        description: "Run drush inside the container.",
+        aliases: ["drush", "dr"],
+      }
+    ];
+  }
+
+  _getWeb() {
+    return this._env.services.firstOfGroup("web");
+  }
+
+  runOperation(op, args) {
+    switch (op) {
+      case "drush":
+        const drushCmd = `--root=${this._getWeb().getDocumentRoot()} ${args.join(" ")}`;
+
+        return this._env.getContainer("docker")
+          .command(drushCmd, ["run", "--rm"], this.ann("id"))
+          .catch((err) => {
+            console.error(err);
+          });
+      default:
+        return super.runOperation(op, args);
+    }
+  }
+
+}
+
+module.exports = DrushService;
