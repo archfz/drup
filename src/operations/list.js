d18db05733333db1ae2109b4836948de6daec1ad
FodorZoltan <fodor18zoltan@gmail.com>
2017-03-06 23:02:46 +0200

    Implements operation to list installed projects.

--- src/operations/list.js
+++ src/operations/list.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const formatter = require("../terminal-utils/formatter");
+
+const ProjectStorage = require("../projects/storage");
+
 module.exports = {
   description : "List all environments and their state.",
   aliases: ["list", "ls"],
@@ -13,6 +17,27 @@ module.exports = {
   ],
 
   execute : () => {
+
+    ProjectStorage.getAll()
+      .then((projects) => {
+        let listByType = {};
+
+        for (let [key, data] of Object.entries(projects)) {
+          if (!listByType[data.type]) {
+            listByType[data.type] = [];
+          }
+
+          listByType[data.type].push(key.green + " : " + data.config.name);
+        }
+
+        console.log("-- List of installed projects");
+        for (let [type, list] of Object.entries(listByType)) {
+          console.log("> " + type.toUpperCase().cyan + " projects");
+          formatter.list(list);
+          console.log();
+        }
+
+      });
 
   }
 };
